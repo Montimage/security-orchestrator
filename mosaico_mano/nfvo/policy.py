c32d56d5406b4795115a0aa7f93b9ba9c1de6099
Hoang Long Mai <hoanglong00792@gmail.com>
2020-12-13 21:03:00 +0100

    fix bug

--- mosaico_mano/nfvo/policy.py
+++ mosaico_mano/nfvo/policy.py
@@ -7,25 +7,25 @@ class Policy(object):
         self.nfvo=nfvo
         self.sdnc=sdnc
     
-    def isPolicyTriggered(condition_json):
+    def isPolicyTriggered(self,condition_json):
         isTriggered=False
         if(self.policy['condition']["triggred_by"]==condition_json["triggred_by"] and self.policy['condition']["metric"]==condition_json["metric"] and self.policy['condition']["value"]==condition_json["value"]):
             isTriggered=True
         return isTriggered
 
-    def triggerPolicy():        
+    def triggerPolicy(self):        
         if(self.policy["target"]=="nfvo"):
-            nfvoAction(self.policy['action'])
+            self.nfvoAction(self.policy['action'])
         elif(self.policy["target"]=="sdnc"):
-            sdncAction(self.policy['action'])
+            self.sdncAction(self.policy['action'])
         
 
-    def nfvoAction():        
+    def nfvoAction(self):        
         if(self.policy['action']["type"]=="deploy_ns_instance"):
             self.nfvo.deploy_ns_instance(self.policy['action']["nsd_name"])                
         
 
-    def sdncAction():
+    def sdncAction(self):
         if(self.policy['action']["type"]=="add_rule_sdnc"):            
             self.sdnc.add_rule(self.policy['action']["deviceId"],self.policy['action']["mac"],self.policy['action']["port"])
         
