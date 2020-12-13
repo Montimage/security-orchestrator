d9fb9c05ab9b3ed484ba52150dca80771bfecf0a
Hoang Long Mai <hoanglong00792@gmail.com>
2020-12-13 18:26:16 +0100

    fix bug

---
+++ mosaico_mano/nfvo_server.py
@@ -0,0 +1,20 @@
+import subprocess
+import requests
+import json
+import time
+import sys
+import yaml
+from nfvo.orchestrator import Orchestrator
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS, cross_origin
+
+nfvo=Orchestrator("osm","vim-emu",None)
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/api/deploy', methods=['POST'])
+def post_api_deploy():
+    data=request.json        
+    nfvo.deploy_security_ns(data["tosca"])
